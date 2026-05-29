949571a8a3967a30c9ad1e3596b3d190cf520541
Dimitrii Chimidov <cimidovdmitrii@gmail.com>
2026-05-30 02:37:56 +0300

    add src/

---
+++ src/database.py
@@ -0,0 +1,30 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy import URL, create_engine, text
+from config import settings
+import asyncio
+
+sync_engine = create_engine(
+    url=settings.DATABASE_URL_psycopg,
+    echo=False,
+    pool_size=5,
+    max_overflow=10
+)
+
+async_engine = create_async_engine(
+    url=settings.DATABASE_URL_asyncpg,
+    echo=False,
+    pool_size=5,
+    max_overflow=10
+)
+
+#with sync_engine.connect() as conn:
+#    res = conn.execute(text("SELECT 1,2,3 union select 4,5,6"))
+#    print(f"{res.first()=}") 
+    
+async def get_123():
+    async with async_engine.connect() as conn:
+        res = await conn.execute(text("SELECT 1,2,3 union select 4,5,6"))
+        print(f"{res.first()=}")
+        
+asyncio.run(get_123())
