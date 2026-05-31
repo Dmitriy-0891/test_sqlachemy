005d283be73076b39b418162a004d8e96381060c
Dimitrii Chimidov <cimidovdmitrii@gmail.com>
2026-06-01 00:31:33 +0300

    Change src/database.py: addstr_256(Annotated), change class Base(str_256)

--- src/database.py
+++ src/database.py
@@ -1,7 +1,8 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import Session, sessionmaker, DeclarativeBase
-from sqlalchemy import URL, create_engine, text
+from sqlalchemy import URL, create_engine, text, String
 from config import settings
+from typing import Annotated
 import asyncio
 
 sync_engine = create_engine(
@@ -21,6 +22,9 @@ async_engine = create_async_engine(
 session_factory = sessionmaker(sync_engine)
 async_session_factory = async_sessionmaker(async_engine)
 
+str_256 = Annotated[str, 256]
 
 class Base(DeclarativeBase):
-    pass
+    type_annotation_map = {
+        str_256: String(256)
+    }
