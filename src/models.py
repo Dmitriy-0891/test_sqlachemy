ee5efe51e1e7cdd25e2149c365f330254f4ed868
Dimitrii Chimidov <cimidovdmitrii@gmail.com>
2026-05-31 23:21:38 +0300

    change src/models.py: add cclass WorkersOrm

--- src/models.py
+++ src/models.py
@@ -1,4 +1,37 @@
 from sqlalchemy import Table, Column, Integer, String, MetaData
+from sqlalchemy.orm import Mapped, mapped_column
+from database import Base
+
+
+class WorkersOrm(Base):
+    __tablename__= "workers"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column()
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 metadata_obj = MetaData()
 
