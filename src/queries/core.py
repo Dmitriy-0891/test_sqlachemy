98e53674cf563d08bcd0461fc5ec0f33a4695c03
Dimitrii Chimidov <cimidovdmitrii@gmail.com>
2026-06-05 00:46:23 +0300

    change src/queries/core.py: add class Syncore: @staticmethod: create_tables; insert_workers; select_workers; update_worker

--- src/queries/core.py
+++ src/queries/core.py
@@ -1,4 +1,4 @@
-from sqlalchemy import text, insert
+from sqlalchemy import text, insert, select, update
 from database import sync_engine, async_engine
 from models import metadata_obj, workers_table
 
@@ -14,22 +14,57 @@ async def get_123():
         print(f"{res.first()=}")
         
         
+class SyncCore:
+    @staticmethod
+    def create_tables():
+        sync_engine.echo = False
+        metadata_obj.drop_all(sync_engine)
+        metadata_obj.create_all(sync_engine)
+        sync_engine.echo = True
 
-def create_tables():
-    metadata_obj.drop_all(sync_engine)
-    metadata_obj.create_all(sync_engine)
+    @staticmethod
+    def insert_workers():
+        with sync_engine.connect() as conn:
+            #stmt = """ INSERT INTO workers (username) VALUES
+            #    ('Jack'),
+            #    ('Michael');"""
+            stmt = insert(workers_table).values(
+                [
+                    {"username": "Jack"},
+                    {"username": "Michael"},
+                ]
+            )
+            conn.execute(stmt)
+            conn.commit()
+        
+    @staticmethod
+    def select_workers():
+        with sync_engine.connect() as conn:
+            query = select(workers_table) # SELECT * FROM workers
+            result = conn.execute(stmt)
+            workers = result.all()
+            print(f"{workers =}")
+            
+    @staticmethod
+    def update_worker(worker_id: int = 2, new_username: str = "Misha"):
+        with sync_engine.connect() as conn:
+            #stmt = text("UPDATE workers SET username=:new_username WHERE worker_id=:id")
+            #stmt = stmt_bindparams(username=new_username, id = worker_id)
+            stmt = (
+                update(workers_table)
+                .values(username=new_username)
+                #.where(workers_table.c.id==worker_id)
+                .filter_by(id=worker_id)
+            )
+            conn.execute()
+            conn.commit()
+
+
+class AsyncCore:
     
-
-def insert_data():
-    with sync_engine.connect() as conn:
-        #stmt = """ INSERT INTO workers (username) VALUES
-        #    ('Bobr'),
-        #    ('Volk');"""
-        stmt = insert(workers_table).values(
-            [
-                {"username": "Bobr"},
-                {"username": "Volk"},
-            ]
-        )
-        conn.execute(stmt)
-        conn.commit()
+    #Асинхронный вариант 
+    @staticmethod
+    async def create_tables():
+        async with async_engine.begin() as conn:
+            await conn.run_sync(metadata_obj.drop_all)
+            await conn.run_sync(metadata_obj.create_all)
