6361c97a22ca384330219c65cf761f7b50818f55
Dimitrii Chimidov <cimidovdmitrii@gmail.com>
2026-06-07 00:59:23 +0300

    tests select_resumes_avg_compesation

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ sys.path.insert(1, os.path.join(sys.path[0], '..'))
 from queries.core import SyncCore, AsyncCore
 from queries.orm import SyncORM
 
-#SyncORM.create_tables()
+SyncORM.create_tables()
 #SyncCore.create_tables()
 
 SyncORM.insert_workers()
@@ -15,8 +15,10 @@ SyncORM.insert_workers()
 #SyncCore.select_workers()
 #SyncCore.update_worker()
 
-#SyncORM.select_workers()
-#SyncORM.update_worker()
+SyncORM.select_workers()
+SyncORM.update_worker()
+SyncORM.insert_resumes()
+SyncORM.select_resumes_avg_compensation()
 
 #create_tables()
 #asyncio.run(insert_data())
